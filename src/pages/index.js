5b05043187b288066dc13ff7f85d95191baaf278
Himanshu Mishra <himanshu@orkohunter.net>
2021-12-24 23:40:53 +0530

    file case changes
    Signed-off-by: Himanshu Mishra <himanshu@orkohunter.net>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import styled from "styled-components";
 import { graphql } from "gatsby";
-
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
 import About from "../components/About";
